7bad18ab3f818d7e18d893a85e273070cc0b4114
Esteban Baquero <estebens22@gmail.com>
2020-10-18 21:13:57 -0500

    Actualizacion para IOS PWA

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE   = 'static-v1';
+const STATIC_CACHE   = 'static-v2';
 const DYMANIC_CACHE  = 'dynamic-v1';
 const INMUTBLE_CACHE = 'inmutable-v1';
 
